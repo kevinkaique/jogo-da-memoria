9bfe61aec6be8e68a4dd4efd658a18b79f1548be
Kevin Kaique <kevinmatos.cedesp@gmail.com>
2024-12-02 19:54:47 -0300

    Add files via upload

---
+++ js/login.js
@@ -0,0 +1,33 @@
+const form = document.querySelector(".login-form");
+const input = document.querySelector(".login__input");
+const button = document.querySelector(".login__button");
+
+//função para adicionar o botão jogar
+const validateInput = ({target}) => {
+
+if (target.value.length > 2) {
+
+    button.removeAttribute('disabled');
+    return;
+}
+
+    button.setAttribute('disabled', '');
+};
+
+
+//função para guardar o nome no local storage
+const handleSubmit = (event) => {
+
+    event.preventDefault();
+
+    localStorage.setItem("player", input.value);
+    input.value = "";
+
+    window.location = "pages/game.html"
+
+};
+
+
+
+form.addEventListener("submit", handleSubmit);
+input.addEventListener("input", validateInput);
